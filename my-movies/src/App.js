5fabd1672c2511301833074ba5a8ca2054b0e3d2
Danilo Silva <danilo.silva@msn.com>
2023-02-20 14:50:03 -0300

    Roteamento

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
-import { MoviesList } from "./components/MoviesList";
-import { MoviesService } from "./services/MoviesService";
-import { MoviesContainer } from "./styles/MoviesContainer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Home } from "./views/Home";
+import { About } from "./views/About";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const fetchMovies = async () => {
-    const { data } = await MoviesService.getMovies();
-    setMovies(data.results);
-  };
-
-  useEffect(() => {
-    fetchMovies();
-  }, [])
-
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
 
-      <MoviesContainer>
-        <MoviesList movies={movies} />
-      </MoviesContainer>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
