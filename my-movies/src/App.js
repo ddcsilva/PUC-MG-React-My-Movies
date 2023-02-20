a0cc8f7e15ff118afeee2a4b8c7bd5a5c3bf66fa
Danilo Silva <danilo.silva@msn.com>
2023-02-20 14:11:49 -0300

    Listagem de Filmes

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { MoviesList } from "./components/MoviesList";
+import { MoviesService } from "./services/MoviesService";
 import { MoviesContainer } from "./styles/MoviesContainer";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const { data } = await MoviesService.getMovies();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
+
   return (
     <div className="App">
       <Header />
 
       <MoviesContainer>
-        <MoviesList movies={[]} />
+        <MoviesList movies={movies} />
       </MoviesContainer>
     </div>
   );
